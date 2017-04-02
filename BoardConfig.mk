2af2fa12138def4bbdbfffb7556d5b3535a2b657
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-04-01 22:28:53 -0300

    fix defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ include vendor/lge/e610/BoardConfigVendor.mk
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=m4 androidboot.selinux=permissive
-TARGET_KERNEL_CONFIG := cyanogenmod_m4_nonfc_defconfig
+TARGET_KERNEL_CONFIG := lineage_m4_nonfc_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-androideabi-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
 
